a1fe933669d5832798e893203998d7fa40df317d
onesword0618 <ao.akua.leo@gmail.com>
2020-11-17 23:53:07 +0900

    Update: 設定ファイルの更新を実行した
    配布用に不要なファイルを入れないように.npmignoreを追加
    precommit時に引っかかるので、拡張子を変更した
    スクリプトを変更した

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+/*
+ *     ####  #######   #####   ######              ####    #####   ##   ##  #######   ####      ####
+ *      ##    ##   #  ##   ##  # ## #             ##  ##  ##   ##  ###  ##   ##   #    ##      ##  ##
+ *      ##    ## #    #          ##              ##       ##   ##  #### ##   ## #      ##     ##
+ *      ##    ####     #####     ##              ##       ##   ##  ## ####   ####      ##     ##
+ *  ##  ##    ## #         ##    ##              ##       ##   ##  ##  ###   ## #      ##     ##  ###
+ *  ##  ##    ##   #  ##   ##    ##               ##  ##  ##   ##  ##   ##   ##        ##      ##  ##
+ *   ####    #######   #####    ####               ####    #####   ##   ##  ####      ####      #####
+ */
+
+export default {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['json'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.+(ts|tsx)', '**/?(*.)+(spec|test).+(ts|tsx)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
